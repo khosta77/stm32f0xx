d7f077ced04e204575dc91cd33edbdbba28584ae
Stepan Filimonov <83815254+khosta77@users.noreply.github.com>
2024-01-12 00:42:30 +0300

    Вывод логической 1 на вывод PA4. Этакий Hello, World!

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "stm32f0xx.h"
 
 int main(void) {
-
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    GPIOA->MODER |= GPIO_MODER_MODER4_0;
+    GPIOA->ODR |= GPIO_ODR_4;
     while (1) {
 
     }
-    return 0;
 }
 
 
